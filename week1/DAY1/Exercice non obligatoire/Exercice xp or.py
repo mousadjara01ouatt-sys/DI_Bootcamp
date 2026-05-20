064d0d073ae08ba08dc29b7c278eeea844a17dfd
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-20 06:05:54 +0200

    first commit

---
+++ week1/DAY1/Exercice non obligatoire/Exercice xp or.py
@@ -0,0 +1,10 @@
+#Exercice1
+mois = int(input("Entrez un numéro de mois (1-12) : "))
+if mois >= 3 and mois <= 5:
+    print(" Spring !")
+elif mois >= 6 and mois <= 8:
+    print(" Summer !")
+elif mois >= 9 and mois <= 11:
+    print(" Autumn !")
+else:
+    print(" Winter !")
