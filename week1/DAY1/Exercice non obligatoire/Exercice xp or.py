26c341bbc4b64ab0fd5382f7d2995b3df984efbf
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-20 06:40:29 +0200

    first commit

--- week1/DAY1/Exercice non obligatoire/Exercice xp or.py
+++ week1/DAY1/Exercice non obligatoire/Exercice xp or.py
@@ -19,3 +19,52 @@ for i in range(1, 21, 2):
     
      
 #Exercice3
+nom = input("Entrez votre nom : ")
+while nom != "OUATTARA":
+    nom  = input("Entrez votre nom : ")
+print("Bienvenue, OUATTARA !")
+
+
+#Exercice4
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+
+name = input("Entrez votre nom : ")
+
+if name in names:
+    print(names.index(name))
+else:
+    print("Ce nom ne figure pas dans la liste.")
+
+
+#Exercice5
+n1 = int(input("Entrez le 1er nombre : "))
+n2 = int(input("Entrez le 2ème nombre : "))
+n3 = int(input("Entrez le 3ème nombre : "))
+
+print(f"Le plus grand nombre est : {max(n1, n2, n3)}")
+
+
+
+#Exercice6
+import random
+
+gagnees = 0
+perdues = 0
+
+while True:
+    nombre_aleatoire = random.randint(1, 9)
+    guess = int(input("Entrez un chiffre entre 1 et 9 : "))
+
+    if guess == nombre_aleatoire:
+        print("Gagnant !")
+        gagnees += 1
+    else:
+        print(f"Bonne chance la prochaine fois. Le nombre était {nombre_aleatoire}.")
+        perdues += 1
+
+    continuer = input("Voulez-vous rejouer ? (oui/non) : ")
+    if continuer.lower() != "oui":
+        break
+
+print(f"\nParties gagnées : {gagnees}")
+print(f"Parties perdues : {perdues}")
