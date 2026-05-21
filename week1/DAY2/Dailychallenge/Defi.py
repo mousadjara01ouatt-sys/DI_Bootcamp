dacdd186d3127e2a34753fd7696d4a6e8a9502ba
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-21 05:00:41 +0200

    first commit

--- week1/DAY2/Dailychallenge/Defi.py
+++ week1/DAY2/Dailychallenge/Defi.py
@@ -1,33 +1,37 @@
 #Défi 1
-mot = input("Entrez un mot : ")
+def create_letter_index(word):
+    letter_indices = {}
+    
+    for index, letter in enumerate(word):
+        if letter in letter_indices:
+            letter_indices[letter].append(index)
+        else:
+            letter_indices[letter] = [index]
+    
+    return letter_indices
 
-dictionnaire = {}
-
-for index, lettre in enumerate(mot):
-    if lettre in dictionnaire:
-        dictionnaire[lettre].append(index)
-    else:
-        dictionnaire[lettre] = [index]
-
-print(dictionnaire)
+word = input("Enter a word: ")
+print(create_letter_index(word))
 
 
 #Défi 2
+def find_affordable_items(items, wallet_str):
+    wallet_amount = int(wallet_str.replace("$", "").replace(",", ""))
+    
+    basket = []
+    
+    for item, price in items.items():
+        clean_price = int(price.replace("$", "").replace(",", ""))
+        
+        if clean_price <= wallet_amount:
+            basket.append(item)
+            wallet_amount -= clean_price
+    
+    if basket == []:
+        return "Nothing"
+    else:
+        return sorted(basket)
+
 items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
 wallet = "$300"
-
-argent = int(wallet.replace("$", "").replace(",", ""))
-
-basket = []
-
-for article, prix in items_purchase.items():
-    prix_propre = int(prix.replace("$", "").replace(",", ""))
-    
-    if prix_propre <= argent:
-        basket.append(article)
-        argent -= prix_propre
-
-if basket == []:
-    print("Nothing")
-else:
-    print(sorted(basket))
+print(find_affordable_items(items_purchase, wallet))
