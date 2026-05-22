511e911062b155a07eb5ce2ba311ea7239f0976c
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-22 03:29:37 +0200

    first commit

---
+++ week1/DAY3/Dailychallenge/Defi.py
@@ -0,0 +1,39 @@
+class Farm:
+    def __init__(self, farm_name):
+        self.name = farm_name
+        self.animals = {}
+
+    def add_animal(self, animal_type, count=1):
+        if animal_type in self.animals:
+            self.animals[animal_type] += count
+        else:
+            self.animals[animal_type] = count
+
+    def get_info(self):
+        result = self.name + "'s farm\n\n"
+        for animal, count in self.animals.items():
+            result += animal + " : " + str(count) + "\n"
+        result += "\n    E-I-E-I-0!"
+        return result
+
+    def get_animal_types(self):
+        return sorted(self.animals.keys())
+
+    def get_short_info(self):
+        animal_list = self.get_animal_types()
+        named = []
+        for animal in animal_list:
+            if self.animals[animal] > 1:
+                named.append(animal + "s")
+            else:
+                named.append(animal)
+        return "The " + self.name + "'s farm has " + ", ".join(named) + "."
+
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
+print(macdonald.get_short_info())
