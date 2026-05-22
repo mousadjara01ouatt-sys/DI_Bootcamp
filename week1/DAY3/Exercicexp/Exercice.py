ea32fbf95d430eb74225d5f7b9eb2bf74e797e86
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-22 03:38:02 +0200

    first commit

--- week1/DAY3/Exercicexp/Exercice.py
+++ week1/DAY3/Exercicexp/Exercice.py
@@ -1,5 +1,4 @@
 #Exercice 1
-
 class Cat:
     def __init__(self, cat_name, cat_age):
         self.name = cat_name
@@ -10,71 +9,68 @@ cat2 = Cat("Luna", 9)
 cat3 = Cat("Milo", 3)
 
 def find_oldest_cat(cat1, cat2, cat3):
-    if cat1.age >= cat2.age and cat1.age >= cat3.age:
-        return cat1
-    elif cat2.age >= cat1.age and cat2.age >= cat3.age:
-        return cat2
-    else:
-        return cat3
+    return max([cat1, cat2, cat3], key=lambda cat: cat.age)
 
 oldest_cat = find_oldest_cat(cat1, cat2, cat3)
-print("The oldest cat is " + oldest_cat.name + " and is " + str(oldest_cat.age) + " years old.")
+print(f"The oldest cat is {oldest_cat.name} and is {oldest_cat.age} years old.")
+
 
 #Exercice 2
-
 class Dog:
     def __init__(self, name, height):
         self.name = name
         self.height = height
 
     def bark(self):
-        print(self.name + " goes woof!")
+        print(f"{self.name} goes woof!")
 
     def jump(self):
-        print(self.name + " jumps " + str(self.height * 2) + " cm high!")
+        print(f"{self.name} jumps {self.height * 2} cm high!")
 
 davids_dog = Dog("Rex", 50)
 sarahs_dog = Dog("Bella", 30)
 
-print(davids_dog.name + " is " + str(davids_dog.height) + " cm tall.")
+print(f"{davids_dog.name} is {davids_dog.height} cm tall.")
 davids_dog.bark()
 davids_dog.jump()
 
-print(sarahs_dog.name + " is " + str(sarahs_dog.height) + " cm tall.")
+print(f"{sarahs_dog.name} is {sarahs_dog.height} cm tall.")
 sarahs_dog.bark()
 sarahs_dog.jump()
 
 if davids_dog.height > sarahs_dog.height:
-    print(davids_dog.name + " is the bigger dog.")
+    print(f"{davids_dog.name} is the bigger dog.")
 elif sarahs_dog.height > davids_dog.height:
-    print(sarahs_dog.name + " is the bigger dog.")
+    print(f"{sarahs_dog.name} is the bigger dog.")
 else:
     print("Both dogs are the same size.")
-    
-   
+
+
 #Exercice 3
 class Song:
-        def __init__(self, lyrics):
-         self.lyrics = lyrics
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
 
-        def sing_me_a_song(self):
-         for line in self.lyrics:
+    def sing_me_a_song(self):
+        for line in self.lyrics:
             print(line)
 
 stairway = Song(["There's a lady who's sure", "all that glitters is gold", "and she's buying a stairway to heaven"])
 stairway.sing_me_a_song()
 
-#Exercice 4
 
+#Exercice 4
 class Zoo:
     def __init__(self, zoo_name):
         self.name = zoo_name
         self.animals = []
+        self.grouped_animals = {}
 
     def add_animal(self, *new_animals):
         for animal in new_animals:
             if animal not in self.animals:
                 self.animals.append(animal)
+        self.grouped_animals = self.sort_animals()
 
     def get_animals(self):
         print(self.animals)
@@ -82,6 +78,7 @@ class Zoo:
     def sell_animal(self, animal_sold):
         if animal_sold in self.animals:
             self.animals.remove(animal_sold)
+        self.grouped_animals = self.sort_animals()
 
     def sort_animals(self):
         self.animals.sort()
@@ -94,16 +91,13 @@ class Zoo:
         return groups
 
     def get_groups(self):
-        groups = self.sort_animals()
-        for letter, animals in groups.items():
-            print(letter + ": " + str(animals))
+        for letter, animals in self.grouped_animals.items():
+            print(f"{letter}: {animals}")
 
 
 brooklyn_safari = Zoo("Brooklyn Safari")
-
 brooklyn_safari.add_animal("Giraffe", "Bear", "Baboon", "Cat", "Cougar", "Lion", "Zebra")
 brooklyn_safari.get_animals()
 brooklyn_safari.sell_animal("Bear")
 brooklyn_safari.get_animals()
-brooklyn_safari.sort_animals()
 brooklyn_safari.get_groups()
