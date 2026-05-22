613254de313d3b44fc4c769111cd6b3264092d3a
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-22 03:56:03 +0200

    first commit

--- week1/DAY3/Dailychallenge/Defi.py
+++ week1/DAY3/Dailychallenge/Defi.py
@@ -3,16 +3,17 @@ class Farm:
         self.name = farm_name
         self.animals = {}
 
-    def add_animal(self, animal_type, count=1):
-        if animal_type in self.animals:
-            self.animals[animal_type] += count
-        else:
-            self.animals[animal_type] = count
+    def add_animal(self, **kwargs):
+        for animal_type, count in kwargs.items():
+            if animal_type in self.animals:
+                self.animals[animal_type] += count
+            else:
+                self.animals[animal_type] = count
 
     def get_info(self):
-        result = self.name + "'s farm\n\n"
+        result = f"{self.name}'s farm\n\n"
         for animal, count in self.animals.items():
-            result += animal + " : " + str(count) + "\n"
+            result += f"{animal} : {count}\n"
         result += "\n    E-I-E-I-0!"
         return result
 
@@ -22,18 +23,33 @@ class Farm:
     def get_short_info(self):
         animal_list = self.get_animal_types()
         named = []
+
+        # Liste des pluriels irreguliers
+        irregular_plurals = {
+            "sheep": "sheep",
+            "deer": "deer",
+            "fish": "fish"
+        }
+
         for animal in animal_list:
             if self.animals[animal] > 1:
-                named.append(animal + "s")
+                if animal in irregular_plurals:
+                    named.append(irregular_plurals[animal])
+                else:
+                    named.append(f"{animal}s")
             else:
                 named.append(animal)
-        return "The " + self.name + "'s farm has " + ", ".join(named) + "."
+
+        # On ajoute "and" avant le dernier animal
+        if len(named) > 1:
+            result = ", ".join(named[:-1]) + f" and {named[-1]}"
+        else:
+            result = named[0]
+
+        return f"The {self.name}'s farm has {result}."
 
 
 macdonald = Farm("McDonald")
-macdonald.add_animal('cow', 5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
-macdonald.add_animal('goat', 12)
+macdonald.add_animal(cow=5, sheep=2, goat=12)
 print(macdonald.get_info())
 print(macdonald.get_short_info())
