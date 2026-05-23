1a7e7685ca0ce0250bdc0df5b8ce13e8e61b4c91
OUATTARA ADJARA MOUSTAPHA <mousadjara01ouatt@gmail.com>
2026-05-23 06:12:49 +0200

    first commit

--- week1/DAY 4/Exercice non obligatoire/Exercice xp or.py
+++ week1/DAY 4/Exercice non obligatoire/Exercice xp or.py
@@ -1,137 +1,122 @@
-class Parrot():
-    
-    def fly(self):
-        print("Parrot can fly")
+class BankAccount:
+    def __init__(self, username, password, balance=0):
+        self.balance = balance
+        self.username = username
+        self.password = password
+        self.authenticated = False
 
-    def swim(self):
-        print("Parrot can't swim")
+    def authenticate(self, username, password):
+        if self.username == username and self.password == password:
+            self.authenticated = True
 
-class Penguin():
+    def deposit(self, amount):
+        if not self.authenticated:
+            raise Exception("You must be authenticated to deposit.")
+        if amount <= 0:
+            raise Exception("Deposit amount must be positive.")
+        self.balance += amount
 
-    def fly(self):
-        print("Penguin can't fly")
-
-    def swim(self):
-        print("Penguin can swim")
-
-# common interface
-def flying_test(bird):
-    bird.fly()
-
-
-
-def my_decorator(my_function):    # <-- (4)
-    def inner_decorator():        # <-- (5)
-        print("This happened before!")  # <-- (6)
-        my_function()             # <-- (7)
-        print("This happens after ")    # <-- (10)
-        print("This happened at the end!")    # <-- (11)
-    return inner_decorator
-    # return None
+    def withdraw(self, amount):
+        if not self.authenticated:
+            raise Exception("You must be authenticated to withdraw.")
+        if amount <= 0:
+            raise Exception("Withdrawal amount must be positive.")
+        self.balance -= amount
 
 
-@my_decorator       # <-- (3)
-def my_decorated():    # <-- (2) <-- (8)
-    print("This happened!")   # <-- (9)
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self, username, password, balance=0, minimum_balance=0):
+        super().__init__(username, password, balance)
+        self.minimum_balance = minimum_balance
+
+    def withdraw(self, amount):
+        if not self.authenticated:
+            raise Exception("You must be authenticated to withdraw.")
+        if amount <= 0:
+            raise Exception("Withdrawal amount must be positive.")
+        if self.balance - amount < self.minimum_balance:
+            raise Exception(f"Cannot withdraw. Balance would go below minimum balance of {self.minimum_balance}.")
+        self.balance -= amount
 
 
-my_decorated()    # <-- (1)
+class ATM:
+    def __init__(self, account_list, try_limit):
+        for account in account_list:
+            if not isinstance(account, (BankAccount, MinimumBalanceAccount)):
+                raise Exception("account_list must contain BankAccount or MinimumBalanceAccount instances.")
 
+        try:
+            if try_limit <= 0:
+                raise Exception("try_limit must be positive.")
+        except Exception:
+            try_limit = 2
 
+        self.account_list = account_list
+        self.try_limit = try_limit
+        self.current_tries = 0
+        self.show_main_menu()
 
-import datetime
+    def show_main_menu(self):
+        while True:
+            print("\n--- ATM Main Menu ---")
+            print("1. Login")
+            print("2. Exit")
+            choice = input("Select an option: ")
+            if choice == "1":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                self.log_in(username, password)
+            elif choice == "2":
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid option.")
 
+    def log_in(self, username, password):
+        for account in self.account_list:
+            account.authenticate(username, password)
+            if account.authenticated:
+                self.current_tries = 0
+                self.show_account_menu(account)
+                return
 
-def my_decorator(inner):
-    def inner_decorator(num_copy):
-        print(datetime.datetime.utcnow())
-        inner(int(num_copy) + 1)
-        print(datetime.datetime.utcnow())
-    return inner_decorator
-
-
-@my_decorator
-def decorated(number):
-    print("This happened : " + str(number))
-
-decorated(5)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MyClass(object):
-    count = 0
-
-    def __init__(self, val):
-        self.val = val
-        MyClass.count += 1
-
-    def set_val(self, newval):
-        self.val = newval
-
-    def get_val(self):
-        return self.val
-
-    @classmethod
-    def get_count(cls):
-        return cls.count
-
-object_1 = MyClass(10)
-print("\nValue of object : %s" % object_1.get_val())
-print(MyClass.get_count())
-
-object_2 = MyClass(20)
-print("\nValue of object : %s" % object_2.get_val())
-print(MyClass.get_count())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MyClass(object):
-    count = 0
-
-    def __init__(self, val):
-        self.val = self.filterint(val)
-        MyClass.count += 1
-
-    @staticmethod
-    def filterint(value):
-        if not isinstance(value, int):
-            print("Entered value is not an INT, value set to 0")
-            return 0
+        self.current_tries += 1
+        if self.current_tries >= self.try_limit:
+            print("Maximum login attempts reached. Exiting.")
+            exit()
         else:
-            return value
+            print(f"Invalid credentials. {self.try_limit - self.current_tries} attempt(s) remaining.")
+
+    def show_account_menu(self, account):
+        while True:
+            print(f"\n--- Account Menu | Balance: {account.balance} ---")
+            print("1. Deposit")
+            print("2. Withdraw")
+            print("3. Exit")
+            choice = input("Select an option: ")
+            if choice == "1":
+                try:
+                    amount = int(input("Enter deposit amount: "))
+                    account.deposit(amount)
+                    print(f"Deposited {amount}. New balance: {account.balance}")
+                except Exception as e:
+                    print(f"Error: {e}")
+            elif choice == "2":
+                try:
+                    amount = int(input("Enter withdrawal amount: "))
+                    account.withdraw(amount)
+                    print(f"Withdrew {amount}. New balance: {account.balance}")
+                except Exception as e:
+                    print(f"Error: {e}")
+            elif choice == "3":
+                print("Logging out.")
+                account.authenticated = False
+                break
+            else:
+                print("Invalid option.")
 
 
-a = MyClass(5)
-b = MyClass(10)
-c = MyClass(15)
+acc1 = BankAccount("alice", "pass123", 500)
+acc2 = MinimumBalanceAccount("bob", "secret", 300, minimum_balance=100)
 
-print(a.val)
-print(b.val)
-print(c.val)
-print(a.filterint(100))
+ATM([acc1, acc2], 3)
